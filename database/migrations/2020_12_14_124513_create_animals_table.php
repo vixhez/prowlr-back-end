f22b13acde1a26a895ca4293aa2a36643b7150e0
tom <tomhall4321@gmail.com>
2020-12-14 14:04:52 +0000

    animals table created

---
+++ database/migrations/2020_12_14_124513_create_animals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name", 50);
+            $table->integer("age");
+            $table->text("bio");
+            $table->string("species", 30);
+            $table->integer("likes");
+            $table->integer("dislikes");
+            $table->string("image-url", 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
